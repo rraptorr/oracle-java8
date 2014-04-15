b49f22a0b6367c51335e98822a911293e726f318
Janusz Dziemidowicz <rraptorr@nails.eu.org>
2014-04-15 23:32:09 +0200

    Java 8u5 release

--- prepare.sh
+++ prepare.sh
@@ -17,14 +17,14 @@ unzip -q "$dir"/jce_policy-8.zip -d "$dir"
 tar -czf "$dir"/../oracle-java${major}_${major}.${minor}.orig-UnlimitedJCEPolicyJDK8.tar.gz -C "$dir" UnlimitedJCEPolicyJDK8
 rm -f "$dir"/jce_policy-8.zip
 
-rm -rf "$dir"/i586 "$dir"/jdk1.${major}.0
-tar xf "$dir"/jdk-${major}-linux-i586.tar.gz -C "$dir"
-mv "$dir"/jdk1.${major}.0 "$dir"/i586
-mv "$dir"/jdk-${major}-linux-i586.tar.gz "$dir"/../oracle-java${major}_${major}.${minor}.orig-i586.tar.gz
+rm -rf "$dir"/i586 "$dir"/jdk1.${major}.0_0${minor}
+tar xf "$dir"/jdk-${major}u${minor}-linux-i586.tar.gz -C "$dir"
+mv "$dir"/jdk1.${major}.0_0${minor} "$dir"/i586
+mv "$dir"/jdk-${major}u${minor}-linux-i586.tar.gz "$dir"/../oracle-java${major}_${major}.${minor}.orig-i586.tar.gz
 
-rm -rf "$dir"/x64 "$dir"/jdk1.${major}.0
-tar xf "$dir"/jdk-${major}-linux-x64.tar.gz -C "$dir"
-mv "$dir"/jdk1.${major}.0 "$dir"/x64
-mv "$dir"/jdk-${major}-linux-x64.tar.gz "$dir"/../oracle-java${major}_${major}.${minor}.orig-x64.tar.gz
+rm -rf "$dir"/x64 "$dir"/jdk1.${major}.0_0${minor}
+tar xf "$dir"/jdk-${major}u${minor}-linux-x64.tar.gz -C "$dir"
+mv "$dir"/jdk1.${major}.0_0${minor} "$dir"/x64
+mv "$dir"/jdk-${major}u${minor}-linux-x64.tar.gz "$dir"/../oracle-java${major}_${major}.${minor}.orig-x64.tar.gz
 
 git --git-dir="$dir"/.git archive --format=tar --prefix=oracle_java${major}-${major}.${minor}/ HEAD README.md prepare.sh | gzip > "$dir"/../oracle-java${major}_${major}.${minor}.orig.tar.gz
